df82b4f3e10c6d843b94db3061195abaaeeb082e
Muhamad Rafi Pamungkas <muhamadrafipamungkas@gmail.com>
2024-05-12 01:38:32 +0700

    Increase test coverage

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -9,11 +9,14 @@ contract FundMeTest is Test {
 
     // Make it storage variable to make it accessible on all test function
     FundMe fundMe;
+    address USER = makeAddr("user");
+    uint256 SEND_VALUE = 0.1 ether;
     
     // This will be called before any test
     function setUp() external {
         DeployFundMe deployFundMe = new DeployFundMe();
         fundMe = deployFundMe.run();
+        vm.deal(USER, 10 ether); // Give USER 10 ether
     }
 
     function testMinimumFiveDollars() public view {
@@ -29,5 +32,34 @@ contract FundMeTest is Test {
 
     function testPriceFeedVersionIsAccurate() public view {
         assertEq(fundMe.getVersion(), 4);
+    }
+
+    function testFundFailsWithoutEnoughETH() public {
+        vm.expectRevert(); // Expect next line to got revert
+        fundMe.fund();
+    }
+
+    function testFundUpdatesDataStructure() public {
+        vm.prank(USER); // Prank the USER to be the sender
+        fundMe.fund{value: SEND_VALUE}();
+        uint256 amountFunded = fundMe.getAddressToAmountFunded(address(USER));
+        assertEq(amountFunded, SEND_VALUE);
+    }
+
+    function testAddFunderToArrayOfFunders() public {
+        vm.prank(USER); // Prank the USER to be the sender
+        fundMe.fund{value: SEND_VALUE}();
+
+        address funder = fundMe.getFunder(0);
+        assertEq(funder, address(USER));
+    }
+
+    function testOnlyOwnerCanWithdraw() public {
+        vm.prank(USER); // Prank the USER to be the sender
+        fundMe.fund{value: SEND_VALUE}();
+
+        vm.expectRevert(); // Expect next line to got revert
+        vm.prank(USER); // Prank the USER to be the sender
+        fundMe.withdraw();
     }
 }
